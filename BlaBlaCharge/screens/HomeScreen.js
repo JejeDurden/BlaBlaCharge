55f8f74f154b0fda977369ad2eb17fccfcf4c22f
raphaelghirelli <raphael.ghirelli@hotmail.com>
2017-10-15 10:05:29 +0200

    les couleurs cest plus cool

--- BlaBlaCharge/screens/HomeScreen.js
+++ BlaBlaCharge/screens/HomeScreen.js
@@ -8,10 +8,9 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Button,
 } from 'react-native';
+import Button from 'apsl-react-native-button'
 
-import { MonoText } from '../components/StyledText';
 
  const styles = StyleSheet.create({
         statusBar: {
@@ -34,20 +33,21 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View>
-
           <View style={styles.statusBar} />
-            <Button
+          <Button
+              style={{backgroundColor: 'red'}} textStyle={{fontSize: 18}}
               onPress={() => { this.props.navigation.navigate('Rechercher')}}
-              title="Rechercher"
-            />
-            <Button
+          >
+              Rechercher
+          </Button>
+          <Button
               onPress={() => { this.props.navigation.navigate('Planifier')}}
               title="Planifier"
-            />
-            <Button
+          />
+          <Button
               onPress={() => { this.props.navigation.navigate('Proposer')}}
               title="Proposer"
-            />
+          />
       </View>
     );
   }
